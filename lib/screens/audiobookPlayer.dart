aa2c6f785024161e3332f0ffc0b64b8cad1117c9
sigjak <sigjak@gmail.com>
2021-03-15 12:16:06 +0000

    on with audiobooks

--- lib/screens/audiobookPlayer.dart
+++ lib/screens/audiobookPlayer.dart
@@ -18,7 +18,8 @@ class BookPlayer extends StatefulWidget {
 class _BookPlayerState extends State<BookPlayer> {
   late AudioPlayer _audioPlayer;
   int index = 1;
-
+  late ConcatenatingAudioSource _playList;
+  bool ready = false;
   @override
   void initState() {
     super.initState();
@@ -29,10 +30,11 @@ class _BookPlayerState extends State<BookPlayer> {
   initBooks(index) async {
     final data = Provider.of<DataProvider>(context, listen: false);
     final session = await AudioSession.instance;
-    final _playList = ConcatenatingAudioSource(children: data.playLists[index]);
+    _playList = ConcatenatingAudioSource(children: data.playLists[index]);
 
     await session.configure(AudioSessionConfiguration.speech());
     await _audioPlayer.setAudioSource(_playList);
+    ready = true;
     setState(() {});
   }
 
@@ -48,75 +50,96 @@ class _BookPlayerState extends State<BookPlayer> {
     final data = Provider.of<DataProvider>(context);
     return Scaffold(
       backgroundColor: Colors.grey[400],
-      appBar: AppBar(title: Text('Audioplayer')),
+      appBar: AppBar(
+        title: Text('AudioBooks'),
+        actions: [
+          TextButton.icon(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.black)),
+              onPressed: () {},
+              icon: Icon(Icons.book),
+              label: Text('ChangeBook'))
+        ],
+      ),
       body: Center(
-        child: Column(
-          children: [
-            StreamBuilder<SequenceState?>(
-                stream: _audioPlayer.sequenceStateStream,
-                builder: (_, snapshot) {
-                  final book = snapshot.data;
-
-                  return book != null
-                      ? Column(children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 20),
-                            height: 200,
-                            child: Image(
-                              image: AssetImage(book.sequence[0].tag.artwork),
-                            ),
-                          ),
-                          Text(book.sequence[0].tag.album,
-                              style: TextStyle(fontSize: 20)),
-                          Text(book.sequence[book.currentIndex].tag.title)
-                        ])
-                      : Text('');
-                }),
-            PlayerButtons(_audioPlayer, false),
-            SliderBar(_audioPlayer),
-            SizedBox(
-              height: 5,
-            ),
-            Expanded(
-              child: Column(
+        child: !ready
+            ? CircularProgressIndicator()
+            : Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      'Radio Stations',
-                      style: TextStyle(fontSize: 22),
+                  Row(children: [
+                    Text(
+                      data.namesOfBooks[0].toString(),
                     ),
+                  ]),
+                  StreamBuilder<SequenceState?>(
+                      stream: _audioPlayer.sequenceStateStream,
+                      builder: (_, snapshot) {
+                        final book = snapshot.data;
+
+                        return book != null
+                            ? Column(children: [
+                                Container(
+                                  margin: EdgeInsets.symmetric(vertical: 20),
+                                  height: 200,
+                                  child: Image(
+                                    image: AssetImage(
+                                        book.sequence[0].tag.artwork),
+                                  ),
+                                ),
+                                Text(book.sequence[0].tag.album,
+                                    style: TextStyle(fontSize: 20)),
+                                Text(book.sequence[book.currentIndex].tag.title)
+                              ])
+                            : Text('');
+                      }),
+                  PlayerButtons(_audioPlayer, false),
+                  SliderBar(_audioPlayer),
+                  SizedBox(
+                    height: 5,
                   ),
                   Expanded(
-                    child: ListView.builder(
-                      itemCount: data.stations.length,
-                      itemBuilder: (BuildContext ctx, int index) {
-                        return Card(
-                          margin: EdgeInsets.fromLTRB(16, 2, 16, 0),
-                          child: ListTile(
-                            leading: CircleAvatar(
-                              backgroundImage:
-                                  AssetImage(data.stations[index].logo),
-                            ),
-                            title: Text(data.stations[index].name),
-                            trailing: IconButton(
-                              onPressed: () async {
-                                await _audioPlayer.stop();
-                                initBooks(index);
-                              },
-                              icon: Icon(Icons.play_arrow),
-                            ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'Episodes',
+                            style: TextStyle(fontSize: 22),
                           ),
-                        );
-                      },
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: _playList.length,
+                            itemBuilder: (BuildContext ctx, int index) {
+                              return Card(
+                                margin: EdgeInsets.fromLTRB(16, 2, 16, 0),
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundImage: AssetImage(
+                                        _playList.sequence[0].tag.artwork),
+                                  ),
+                                  title:
+                                      Text(_playList.sequence[index].tag.title),
+                                  trailing: IconButton(
+                                    onPressed: () async {
+                                      await _audioPlayer.stop();
+                                      _audioPlayer
+                                          .setAudioSource(_playList[index]);
+                                      setState(() {});
+                                    },
+                                    icon: Icon(Icons.play_arrow),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 26)
+                      ],
                     ),
-                  ),
-                  SizedBox(height: 26)
+                  )
                 ],
               ),
-            )
-          ],
-        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.grey[600],
